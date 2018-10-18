3f2a9a214891a8a4b89c1291a6aa4ee500f7bb15
Sairama Jamalapuram <sairamaj@hotmail.com>
2018-10-18 22:34:56 +0000

    upadted travis file for dotnet core

--- src/uploadimages/Program.cs
+++ src/uploadimages/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace uploadimages
 {
@@ -6,7 +8,13 @@ namespace uploadimages
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CloudStorageAccount storageAccount;
+            CloudBlobContainer cloudBlobContainer;
+            string sourceFile;
+            string destinationFile = null;
+            
+            string storageConnectionString = Environment.GetEnvironmentVariable("storageconnectionstring");
+            Console.WriteLine(storageConnectionString);
         }
     }
 }
